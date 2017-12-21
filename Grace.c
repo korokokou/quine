c5575d2fd31b6364bdded8ab03ce794727053cad
Korok Toi <abrikou@gmail.com>
2017-12-21 01:05:17 +0100

    oko

--- Grace.c
+++ Grace.c
@@ -1,57 +1,6 @@
-#include <fcntl.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
+#include<stdio.h>
 /*
-* ny
+	c'est chiant
 */
-
-char	*s = "%stoto\n";
-
-int	ft_strlen(char *s)
-{
-	int i;
-
-	i = 0;
-	while(s[i])
-		i++;
-	return (i);
-}
-
-char	*escape(char *s)
-{
-	char *result;
-	char *start;
-
-
-	result = malloc(sizeof(char) * ft_strlen(s));
-	start = result;
-	while (*s)
-	{		if (*s == '\t' || *s == '\n' || *s == '\"' || *s == '\\')
-			{	*result++ = '\\';
-				if (*s == '\t')
-					*result++ = 't';
-				else if (*s == '\n')
-					*result++ = 'n';
-				else if (*s == '\\')
-					*result++ = '\\';
-				else
-					*result++ = '"';
-				s++;
-			}
-			else
-				*result++ = *s++;
-	}
-	return (start);
-}
-
-int main( void )
-{
-	int fd = open("kid.c", O_CREAT);	
-	if (fd != -1)
-	{
-		dprintf(fd, s, "bernard");
-		close(fd);
-	}
-	return (0);
-}
+#define FT(x)int main(){FILE *f=fopen("Grace_kid.c","w");fprintf(f,"#include<stdio.h>\n/*\n\tc'est chiant\n*/\n#define FT(x)%s\nFT(%s)\n", #x, #x);return 0;}
+FT(int main(){FILE *f=fopen("Grace_kid.c","w");fprintf(f,"#include<stdio.h>\n/*\n\tc'est chiant\n*/\n#define FT(x)%s\nFT(%s)\n", #x, #x);return 0;})
--- Makefile
+++ Makefile
@@ -6,9 +6,11 @@
 #    By: takiapo <takiapo@student.42.fr>            +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2013/11/20 21:45:12 by takiapo           #+#    #+#              #
-#*   Updated: 2016/12/19 09:19:01 by                  ###   ########.fr       *#
+#*   Updated: 2017/12/21 01:04:43 by                  ###   ########.fr       *#
 #                                                                              #
 # **************************************************************************** #
+
+SULLY = Sully
 
 COLLEEN = Colleen
 
@@ -26,20 +28,21 @@ CC = gcc
 SRCGRACE = 	\
 		Grace.c
 
-SRCCOLLEEN = 	\
-		Colleen.c 
+SRCSULLY = 	\
+		Sully.c 
 
 INCLUDE = -I includes/
 
 
-all : $(COLLEEN) $(GRACE) 
+all : $(COLLEEN) $(GRACE) $(SULLY)
 
 $(COLLEEN) :  $(SRCCOLEEN)
 	$(CC) -o  $(COLLEEN) $(SRCCOLLEEN) $(LIBFLAG) $(WFLAGS) $(INCLUDE) 
 
 $(GRACE) :  $(SRCGRACE)
 	$(CC) -o  $(GRACE) $(SRCGRACE) $(LIBFLAG) $(WFLAGS) $(INCLUDE) 
-
+$(SULLY) :  $(SRCSULLY)
+	$(CC) -o  $(GRACE) $(SRCGRACE) $(LIBFLAG) $(WFLAGS) $(INCLUDE) 
 clean :
 	echo 'clean'
 
